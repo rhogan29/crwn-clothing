2fc65305973f10dfeb26d558230aea0480b5e63a
Robert Hogan <roberthogan@stargate.local>
2019-08-29 10:46:36 -0400

    implemented firebase utils. Included ability to store authenticated users into firebase database.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,5 @@
 import firebase from 'firebase/app';
-// storage
 import 'firebase/firestore';
-// authentication
 import 'firebase/auth';
 
 // project config 
@@ -15,13 +13,61 @@ const config = {
     appId: "1:23305190889:web:14c8e808282d3c5d"
   };
 
+  // pass the userAuth into this function in app.js
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    // if userAuth doesnt exist, return out
+    if (!userAuth) return;
+
+    // if it exists, set userRef to the .doc of the users collection 
+    // with the proper uid
+    // this is aslo known as a QueryReference
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    // also, get a snapshot of that userrefs information
+    // for later use
+    // this contains the property 'exists'
+    const snapShot = await userRef.get();
+
+    // if the snapshot does not exist, we will create 
+    // the data. If it does, just return the userRef
+    if (!snapShot.exists) {
+      // get the display name and email from the userauth object
+      const { displayName, email } = userAuth;
+      // get the time stamp of when it was created
+      const createdAt = new Date();
+
+      // async request to our database to actually 
+      // store this user info
+      try {
+        // .set will create the user 
+        // pass in all the data you want to have
+        // on the user object
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          // any additional data we might want later 
+          ...additionalData
+        })
+      } catch(error) {
+        console.log('error creating user', error.message);
+      }
+    }
+    return userRef;
+  }
+
+  // ***** initialize firebase *****
   firebase.initializeApp(config);
 
+  // export these, now the rest of the app has access 
+  // if they need auth or database access
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
+  // google sign in provider from firebase
   const provider = new firebase.auth.GoogleAuthProvider();
 
+  // custom parameters below
   // always trigger google pop-up when using google sign in
   provider.setCustomParameters({ prompt: 'select_account' });
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
