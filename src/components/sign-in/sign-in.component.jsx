4c569cdbbb8e0482fd8b9490a3423e4716b30960
Robert Hogan <roberthogan@stargate.local>
2019-08-29 11:01:18 -0400

    Completed sign up and sign in methods, and components with proper authentication using firebase.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 //firebase
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -24,10 +24,18 @@ class SignIn extends React.Component {
         this.setState({ [name]: value });
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: ''});
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            // if succeeds, clear state
+            this.setState({ email: '', password: ''});
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     render() {
